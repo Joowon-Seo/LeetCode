8833ea8432ef3872cb449399ee2614b90b166604
Joowon-Seo <90291431+Joowon-Seo@users.noreply.github.com>
2022-05-26 18:56:14 +0900

    Time: 1 ms (78.47%), Space: 40.8 MB (61.37%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
@@ -1,9 +1,6 @@
 class Solution {
     public int minOperations(int n) {
         
-        int cnt = n%2 == 0 ? (n/2)*(n/2):(n/2)*(n/2 +1);
-
-        return cnt;
-        
+        return n%2 == 0 ? (n/2)*(n/2):(n/2)*(n/2 +1);
     }
 }
