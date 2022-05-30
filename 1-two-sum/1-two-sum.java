039dec4b9d47eebbc5a139c9932a4c67b4baf5d8
Joowon-Seo <90291431+Joowon-Seo@users.noreply.github.com>
2022-05-30 16:48:55 +0900

    Time: 694 ms (5.03%), Space: 45.9 MB (27.61%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,13 +1,21 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> tracker = new HashMap<Integer, Integer>();
-        for(int i = 0; i < nums.length; i++){
-            if(tracker.containsKey(nums[i])){
-                int left = tracker.get(nums[i]);
-                return new int[]{left, i};
-            }else
-                tracker.put(target - nums[i], i);
+        int[] result = new int [2];
+        ArrayList<Integer> numsList = (ArrayList<Integer>) Arrays.stream(nums).boxed().collect(Collectors.toList());
+
+        retry:
+        for (int i = 0; i < numsList.size(); i++) {
+            if (numsList.contains(target - numsList.get(i))){
+                result[0] = i;
+                result[1] = numsList.indexOf(target - numsList.get(i));
+                if (result[0] == result[1]){
+                    continue retry;
+                }
+                break;
+            }
         }
-        return new int[2];
+
+        
+        return result;
     }
 }
