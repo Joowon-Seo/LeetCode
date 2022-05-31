8ba242aa906806d5089369a6dacdc981f62f06b7
Joowon-Seo <90291431+Joowon-Seo@users.noreply.github.com>
2022-05-31 18:30:37 +0900

    Update 19-remove-nth-node-from-end-of-list.java

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
@@ -8,6 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// 현재 노드에서 N번 만큼 다음 노드로 갔을 때의 값이 null 이라면 현재 노드가 삭제되어야 할 노드 입니다.
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         if (head.next == null){
