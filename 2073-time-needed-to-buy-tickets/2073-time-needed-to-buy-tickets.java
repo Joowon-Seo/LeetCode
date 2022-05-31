5a45e44f667fcdba116c2a0125b02592446b614a
Joowon-Seo <90291431+Joowon-Seo@users.noreply.github.com>
2022-05-31 17:09:59 +0900

    Time: 3 ms (33.65%), Space: 39.7 MB (93.55%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,30 +1,12 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
+       return IntStream.range(0, tickets.length).map(i -> Math.min(tickets[i], i > k ? tickets[k] -1  : tickets[k])).sum();
+        // i가 k 보다 앞에 있다면 최소 k가 0이 되거나 해당 인덱스의 값이 0이 될 때 까지 봐야함으로
+        // min(tickets[i], tickets[k])
         
-        Deque<Integer> deque = new ArrayDeque();
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < tickets.length; i++) {
-
-            map.put(i, tickets[i]);
-            deque.add(i);
-        }
-
-        int time = 0;
-
-        while (map.get(k) != 0){
-            if (map.get(deque.peek()) == 0){
-                deque.poll();
-                continue;
-            }
-            map.put(deque.peek(), map.get(deque.peek()) - 1);
-            deque.add(deque.poll());
-            time ++;
-
-
-        }
-
-        return time;
-        
+        // i > k 라면 i 부분이 인덱스가 0이 될 때 k는 가장 뒤에 있게됨 tickets[k] - 1상태로
+        // 여기서 i < k 와 같이 해석하면 같음
+        // 따라서 min(tickets[i], tickets[k] - 1)이 되는 것임
     }
+       
 }
