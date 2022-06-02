aa05c943f10d257ff64486cc9174f64f20ca9cd8
Joowon-Seo <90291431+Joowon-Seo@users.noreply.github.com>
2022-06-03 06:52:02 +0900

    Time: 10 ms (67.04%), Space: 68.4 MB (60.66%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -2,13 +2,11 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
          Set<Integer> set = new HashSet<>();
         for (int i : nums){
-            set.add(i);
+            if (!set.add(i)){
+                return true;
+            }
         }
 
-        if (set.size() != nums.length){
-            return true;
-        }
-        
         return false;
     }
 }
